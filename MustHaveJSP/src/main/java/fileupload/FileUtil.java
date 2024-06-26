3636664658a3a3b684f55f8cfd702958afafa84d
2eemj000 <2eemj000@gmail.com>
2024-06-26 17:55:22 +0900

    0626 수정

---
+++ MustHaveJSP/src/main/java/fileupload/FileUtil.java
@@ -0,0 +1,64 @@
+package fileupload;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Part;
+
+public class FileUtil{
+	//파일 업로드
+	//요청 객체 내에 전송된 파일을 디스크에 저장하고 파일명을 리턴
+	public static String uploadFile(HttpServletRequest req, String sDirectory)
+			throws ServletException, IOException{ //예외처리
+		Part part = req.getPart("ofile"); //request 내장객체의 getPart()메서드로 file타입으로 전송된 폼값받아 Part객체에 저장
+		String partHeader = part.getHeader("content-disposition"); //content-disposition 헤더값 읽어옴 (name속성, 파일명 포함된 상태)
+		
+		//[partHeader=form-data; name="attachedFile"; fileName="파일명.jpg"] 
+		String[] phArr = partHeader.split("filename="); //split()으로 분리
+		String originalFileName = phArr[1].trim().replace("\"",""); //공백제거, 쌍따옴표도 제거
+		
+		//전송된 파일이 있다면 디렉터리에 파일저장
+		if (!originalFileName.isEmpty()) {
+			part.write(sDirectory+File.separator+originalFileName);
+		}
+		return originalFileName;
+	}
+	//파일명 변경
+	public static String renameFile(String sDirectory, String fileName) {
+		String ext = fileName.substring(fileName.lastIndexOf("."));
+		String now = new SimpleDateFormat("yyyyMMdd_HmsS").format(new Date()); //new Date()는 현재시간데이터를 만듦
+		String newFileName = now+ext; //파일명과 확장자 연결
+		File oldFile = new File(sDirectory+File.separator+fileName); 
+		File newFile = new File(sDirectory+File.separator+newFileName);
+		oldFile.renameTo(newFile); //원본파일과 새로운파일에 대한 File객체 생성 후 파일명을 변경
+		
+		//원본 파일명 반환
+		return newFileName;
+	}
+	//multiple 속성 추가로 2개 이상의 파일 업로드
+	public static ArrayList<String> multipleFile(HttpServletRequest req, String sDirectory)
+			throws ServletException, IOException{
+		
+		ArrayList<String> listFileName = new ArrayList<>();
+		Collection<Part> parts = req.getParts();
+		for(Part part : parts) {
+			if(!part.getName().equals("ofile"))
+				continue;
+			String partHeader = part.getHeader("contene-disposition");
+			String[] phArr = partHeader.split("filename=");
+			String originalFileName = phArr[1].trim().replace("\"","");
+			if (!originalFileName.isEmpty()) {
+				part.write(sDirectory+File.separator +originalFileName);
+			}
+			listFileName.add(originalFileName);
+ 		}
+		return listFileName;
+	}
+}
---
+++ MustHaveJSP/src/main/java/fileupload/MyFileDAO.java
@@ -0,0 +1,70 @@
+package fileupload;
+
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Vector;
+
+import common.JDBConnect;
+
+public class MyFileDAO extends JDBConnect{ // JDBC를 통해 DB에 연결
+	
+//	public static void main(String[] args) {
+//		MyFileDAO m = new MyFileDAO();
+//		MyFileDTO dto = new MyFileDTO();
+//		dto.setTitle("Title");
+//		dto.setOfile("aa");
+//		dto.setSfile("bb");
+//		m.insertFile(dto);
+//	}
+	
+	// 새로운 게시물을 입력합니다.
+	public int insertFile(MyFileDTO dto) { // 새로운 게시물 입력을 위한 메서드
+		PreparedStatement psmt = null;
+		int applyResult = 0;
+		try {
+			String query = "INSERT INTO myfile (title, cate, ofile, sfile) "
+						 + "VALUES (?,?,?,?)";
+			psmt = getCon().prepareStatement(query);
+			psmt.setString(1, dto.getTitle());
+			psmt.setString(2, dto.getCate());
+			psmt.setString(3, dto.getOfile());
+			psmt.setString(4, dto.getSfile());
+			applyResult = psmt.executeUpdate(); // 쿼리문을 실행하면 적용된 행의 개수가 정수로 반환 (1이면 성공, 0이면 실패)
+		}
+		catch (Exception e) {
+			System.out.println("INSERT 중 예외 발생");
+			e.printStackTrace();
+		}
+		return applyResult;
+	}
+	
+	// 파일목록을 반환합니다.
+	public List<MyFileDTO> myFileList(){
+		List<MyFileDTO> fileList = new Vector<MyFileDTO>();
+		
+		// 쿼리문 작성
+		String query = "SELECT * FROM myfile ORDER BY idx DESC";
+		try {
+			Statement stmt = getCon().createStatement(); // 쿼리문 생성
+			ResultSet rs = stmt.executeQuery(query); // 쿼리 실행
+			
+			while (rs.next()) {
+				MyFileDTO dto = new MyFileDTO();
+				dto.setIdx(rs.getString(1));
+				dto.setTitle(rs.getString(2));
+				dto.setCate(rs.getString(3));
+				dto.setOfile(rs.getString(4));
+				dto.setSfile(rs.getString(5));
+				dto.setPostdate(rs.getString(6));
+				
+				fileList.add(dto);
+			}
+		} catch (Exception e) {
+			System.out.println("SELECT 시 예외 발생");
+			e.printStackTrace();
+		}
+		return fileList;
+	} 
+}
