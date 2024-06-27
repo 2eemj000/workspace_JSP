b94889f07e870f59f168c807c98b04fd6f74d276
2eemj000 <2eemj000@gmail.com>
2024-06-27 17:58:15 +0900

    0627 수정

--- MustHaveJSP/src/main/java/utils/JSFunction.java
+++ MustHaveJSP/src/main/java/utils/JSFunction.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.jsp.JspWriter;
 
 public class JSFunction {
@@ -22,6 +25,19 @@ public class JSFunction {
 					+"	history.back();"
 					+"</script>";
 			out.println(script);
+		}
+		catch (Exception e) {}
+	}
+	// 메시지 알림창을 띄운 후 명시한 URL로 이동
+	public static void alertLocation(HttpServletResponse resp, String msg, String url) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = resp.getWriter();
+			String script = ""
+						  + "<script>"
+						  + "	alert('" +msg+"');"
+						  + "</script>";
+			writer.print(script);
 		}
 		catch (Exception e) {}
 	}
